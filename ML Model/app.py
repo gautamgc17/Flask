9919c53cf1331c5e97e9caf8cc2b025b79b4fda0
gautamgc17 <71714942+gautamgc17@users.noreply.github.com>
2021-05-14 23:27:10 +0530

    Add files via upload

---
+++ ML Model/app.py
@@ -0,0 +1,25 @@
+from flask import Flask , render_template , request , redirect
+import joblib as jb
+
+app = Flask(__name__)
+
+model = jb.load("lin_reg_model.pkl")
+
+@app.route('/')
+def display():
+	return render_template('index.html')
+
+# send form data/request on same route
+@app.route('/' , methods=['POST'])
+def marks():
+
+	if request.method == 'POST':
+		
+		hour = float(request.form['hours'])
+		marks = str(model.predict([[hour]])[0][0])
+
+	return render_template('index.html' , your_marks = marks)
+
+
+if __name__ == '__main__':
+	app.run(debug =True)
