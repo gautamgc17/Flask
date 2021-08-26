cf52e5b4f3363dac8728feafabe3a01ebd4cf405
gautamgc17 <71714942+gautamgc17@users.noreply.github.com>
2021-08-26 12:24:22 +0530

    Update app.py

--- Flask Basics/app.py
+++ Flask Basics/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
-app = Flask(__name__)   # object of Flask class is our WSGI application
+app = Flask(__name__)   # create object of Flask class ; This is the WSGI application
 
-@app.route('/')   # decorator which tells the application which URL should call the associated function responsible for returning the server response
+@app.route('/')    # decorator which tells the application which URL should call the associated function responsible for returning the server response
 def display():
 	return "This is our first Flask application😉✌"
+
+@app.route('/new')        # different routes with different binding functions
+def new():
+	return 'This is another web page!!'
 
 if __name__ =='__main__':
 	app.run(debug = True)
